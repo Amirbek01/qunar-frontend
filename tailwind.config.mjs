011fa8f7cf2ae62802115d99c13425d5b8c97900
Amirbek01 <113281334+Amirbek01@users.noreply.github.com>
2025-02-22 12:57:16 +0500

    create ang frist page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,12 +4,21 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,jsx,ts,tsx}',
+
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+    },
+  },
+  theme: {
+    extend: {
+      colors: {
+        'custom-dark': '#444444',
       },
     },
   },
